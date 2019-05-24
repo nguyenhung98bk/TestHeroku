d1882142d43d238614f4599c1280bebe9923501b
nguyenhung98bk <48485798+nguyenhung98bk@users.noreply.github.com>
2019-05-24 18:02:43 +0700

    update

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="container">
+    <div class="header">
+    	<div class="header-content">
+    		<div class="icon">
+    			<input type="text" name="" class="search">
+    			<i class="fa fa-search" aria-hidden="true"></i>
+    		</div>
+            <div class="select"><button>ALL</button><i class="fa fa-angle-down fa-lg" aria-hidden="true"></i></div>
+    	</div>
+    	<div class="menu-header">
+    		<div class="menu">
+    			<ul>
+    				<li><div><a href="">Departments</a><i class="fa fa-angle-down fa-lg" aria-hidden="true"></i></div></li>
+    				<li><a href="">Acount</a></li>
+    				<li><a href="">Deals</a></li>
+    				<li><a href="">Registry</a></li>
+    				<li><a href="">Sell</a></li>
+    				<li><a href="">Help</a></li>
+    			</ul>
+    		</div>
+    		<div class="menu2">
+    			<ul>
+    				<li><div><a href="">Sign in</a><i class="fa fa-angle-down fa-lg" aria-hidden="true"></i></div></li>
+    				<li><a href="">Cart</a></li>
+    			</ul>
+    			<img src="https://cdn.shopify.com/s/files/1/1270/2753/products/red_large_large_a78485cd-0b1c-467d-a8ac-9dd0d1c13c38_large.jpg?v=1516910139" width="50px" height="50px" alt="Cart">
+    		</div>
+    	</div>
+    </div>
+    <div class="content">
+    	<div class="content-header">
+    		<h2>Clothing Shop</h2>
+    		<p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit. Etiam<br>efficitur sapien ornare lectus<br>vehicula dapibus.
+    		</p>
+    	</div>
+    	<div class="content-main">
+    		<div class="Welcome">
+    			<h3>Welcome</h3>
+    				<button id="note">Sign in</button>
+    			<button id="note">Create account</button>
+    		</div>
+    		<div class="Popular">
+    			<h3>Popular Departments</h3>
+    			<div class="images">
+    				<img src="https://i.etsystatic.com/9563100/r/il/cb3784/628987319/il_794xN.628987319_jbw0.jpg" width="90px" height="60px">
+    				<img src="https://i5.walmartimages.com/asr/d93621af-9952-4b1a-ab21-55bb0617eff5_1.a075d3bf6a3c1b1be4418d00f61e2a1b.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF" width="90px" height="60px">
+    				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK_L-XloX-QyEnYM4l7nlOVVxjTYQmNJCKRnFJCtLARK2EYem6" width="90px" height="60px">
+    				<img src="https://cdn.shopify.com/s/files/1/0789/9713/products/1221-J-Bravest-Firefighter-With-Maltese_large.jpg?v=1548356008" width="90px" height="60px">
+    				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZLpY-qaY6rvVkk50aA15BeWuUg2tFMjcS9s_uDfzAziFkRaV7" width="90px" height="60px">
+    				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJEEAlDa7gHm6k70XFSF5k5Rq211yTnG68FF2LCJTYQh-TLiwNXQ" width="90px" height="60px">
+    			</div>
+    		</div>
+    		<div class="Clothing">
+    			<h3>Clothing</h3>
+    			<img src="https://thumbs.dreamstime.com/z/fashion-shop-window-clothing-store-front-view-womens-woman-mannequin-retail-63352967.jpg" width="180px" height="195px">
+    			<button id="note">Shop Clothing</button>
+    		</div>
+    		<div class="Deals">
+    			<h3>Deals</h3>
+    			<img src="https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555359433/shape/mentalfloss/shop-it.png" width="180px" height="160px">
+    			<p >Price</p>
+    			<button id="note">Shop Deals</button>
+    		</div>
+    	</div>
+    </div>
+    	<button class="content-footer">Return to top</button>
+    <div class="footer">
+    	<div class="fo">
+    		<h3>About</h3>
+    		<p>Lorem</p>
+    		<p>Ipsum</p>
+    		<p>Dolor</p>
+    	</div>
+    	<div class="fo">
+    		<h3>Jobs</h3>
+    		<p>Lorem</p>
+    		<p>Ipsum</p>
+    		<p>Dolor</p>
+    	</div>
+    	<div class="fo">
+    		<h3>Payment products</h3>
+    		<p>Lorem</p>
+    		<p>Ipsum</p>
+    		<p>Dolor</p>
+    	</div>
+    	<div class="fo">
+    		<h3>Help</h3>
+    		<p>Lorem</p>
+    		<p>Ipsum</p>
+    		<p>Dolor</p>
+    	</div>
+    </div>
+</div>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1.Tiêu đề bài tập: 
+Làm giao diện theo wireframe
+Họ tên: Nguyễn Việt Hưng
+2.Liên kết:
+Liên kết đề bài: https://www.lucidchart.com/pages/templates/wireframe/ecommerce-wireframe-template
+Liên kết học liệu: http://tutorials.codebar.io
+Liên kết bài làm online:
+3.Hướng dẫn cài đặt test/sử dụng nếu có:
+4.Kiến thức nắm được:
